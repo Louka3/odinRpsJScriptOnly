8c0c9fd102bc85a458a5108906efccc315b5efba
Louka3 <lkuczykowski@gmail.com>
2022-02-05 20:46:59 -0500

    Had to rework a lot of previous code to get the logic to sync up with what the project called for in the GUI. Game is complete. Doesn't look pretty but it works :D

--- script.js
+++ script.js
@@ -1,74 +1,31 @@
-// function getRandomNum(){
-//     return (Math.floor(Math.random() * 3) + 1)
-//     /*https://www.w3schools.com/js/js_random.asp
-//     had to look up how to generate a random number */
-// }
-// function computerPlay(){
-//     /*Assigns either rock paper or scissors depending on the random number 
-//     returned from getRandomNum */
-//     let num = getRandomNum()
-//     switch(num) {
-//         case 1:
-//             return "Rock"
-//         case 2:
-//             return "Paper"
-//         case 3:
-//             return "Scissors"
-//         default:
-//             return "Nope"
-//         }
-// }
-// function playerSelection(){
-//     let selection = window.prompt("","Rock, Paper, or Scissors?: ")
-//     return (selection.slice(0,1).toUpperCase() + selection.slice(1,selection.length).toLowerCase())
-//             /*Ensures the returned choice has the proper casing */
-// }
-// function getComputerSelection(){
-//     return computerPlay()
-// }
-// function getPlayerSelection(){
-//     return playerSelection()
-// }
-
-// function playGame(){
-//     let player = getPlayerSelection()
-//     let computer = getComputerSelection()
-//     let winner = getWinner(player, computer)
-    
-//     if (winner == "Tie"){
-//         return ("The player chose " + player + ".\nThe computer chose " +
-//         computer + ".\nThe game was a tie!")
-//     }
-//     return ("The player chose " + player + ".\nThe computer chose " +
-//     computer + ".\nThe " + winner + " is the winner!")
-//     //end
-// }
-
-const newGameButton = document.querySelector('#new-game')
+//query selector variables
 const rockButton = document.querySelector('#rock')
 const paperButton = document.querySelector('#paper')
 const scissorsButton = document.querySelector('#scissors')
 const display = document.querySelector('#display')
-const buttonArray = [rockButton, paperButton, scissorsButton]
-let selection = ""
-let computerSelection = ""
+const playerScoreDisplay = document.querySelector('#player-score')
+const computerScoreDisplay = document.querySelector('#computer-score')
+//variables
+let computerSelection = computerChoice()
 let winner = ""
+let playerScore = 0
+let computerScore = 0
+//event listeners for buttons
+rockButton.addEventListener('click',playGame.bind(this,'Rock'))
+paperButton.addEventListener('click', playGame.bind(this,'Paper'))
+scissorsButton.addEventListener('click', playGame.bind(this,'Scissors'))
 
-newGameButton.addEventListener("click",playGame)
 
-function playGame() {
-    selection = ""
-    computerSelection = ""
-    winner = ""
-    display.textContent = "Select Rock, Paper, or Scissors";
-    computerSelection = computerChoice();
-    rockButton.addEventListener('click', getWinner.bind(this,'Rock', computerSelection))
-    paperButton.addEventListener('click', getWinner.bind(this,'Paper', computerSelection))
-    scissorsButton.addEventListener('click', getWinner.bind(this,'Scissors', computerSelection))
-    for (let b = 0; b < buttonArray.length; b++){
-        buttonArray[b].addEventListener('click',displayWinner)
-    }
+function playGame(playerSelection) {
+    winner = getWinner(playerSelection, computerSelection)
+    announceWinner(winner, playerSelection, computerSelection)
+    changeScore(winner)
+    displayScore()
+    //get a new selection for the computer
+    computerSelection = computerChoice()
+    winner = ""   
 }
+
 function getSelection(playerSelection){
     selection = playerSelection
 }
@@ -95,11 +52,6 @@ function computerChoice(){
         }
 }
 
-function displayWinner() {
-    
-    display.textContent = "The winner is: " + winner;
-}
-
 function getWinner(player, computer){
     /*Uses switch statements to determine the winner of the game.
     Seperate switch statements in each case for each possible player answer.
@@ -110,38 +62,60 @@ function getWinner(player, computer){
         case "Rock":
             switch (computer){
                 case "Paper":
-                    winner = "Computer"
-                    break
+                    return "Computer"
                 case "Scissors":
-                    winner = "Player"
-                    break
+                    return "Player"
                 default: 
-                    winner = "Tie"
-                    break
+                    return "Tie"
             }
         case "Paper":
             switch (computer){
                 case "Rock":
-                    winner = "Player"
-                    break
+                    return "Player"
                 case "Scissors":
-                    winner = "Computer"
-                    break
+                    return "Computer"
                 default:
-                    winner = "Tie"
-                    break
+                   return "Tie"
             }
         case "Scissors":
             switch (computer){
                 case "Rock":
-                    winner = "Computer"
-                    break
+                    return "Computer"
                 case "Paper":
-                    winner = "Player"
-                    break
+                    return "Player"
                 default:
-                    winner = "Tie"
-                    break
+                    return "Tie"
             }
     }
+}
+
+function changeScore(win){
+    if (win == 'Player'){
+        ++playerScore
+        playerScoreDisplay.innerText = "Player Score: " + playerScore
+    }
+    if (win == 'Computer'){
+        ++computerScore
+        computerScoreDisplay.innerText = "Computer Score: " + computerScore
+    }
+}
+
+function displayScore(){
+    playerScoreDisplay.innerText = "Player Score: " + playerScore;
+    computerScoreDisplay.innerText = "Computer Score: " + computerScore;
+}
+
+function announceWinner(win, play, comp){
+    if (win == 'Tie'){
+        display.innerText = "Player Choice: " + play + 
+            "\tComputer Choice: " + comp + "\r\nThe game was a tie!"
+        return null
+    } 
+    display.innerText = "Player Choice: " + play + 
+    "\tComputer Choice: " + comp + "\r\nThe winner is: " + win
+    /*The only way for me to add newline and tab characters into the string used in .innerText
+    was to first change the css if the dispaly element and add the attribute "white-space: pre;"
+    (pre-line; is also supposed to work). After adding that to the css, my '\t' was working. 
+    Newlines have to use '\r\n'. Every other method I saw either required more work or required me
+    to use .innerHTML, which I wanted to avoid since I've read it could lead to security issues.*/
 }
